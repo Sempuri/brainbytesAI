999a4e784d5e61a14056f3e42a756ed63b067672
Sempuri <lr.recristobal@mmdc.mcl.edu.ph>
2025-06-24 19:32:32 +0800

    silence 'global is not defined' lint error in jest.setup.js

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { Headers } from "node-fetch";
 if (typeof global.Headers === "undefined") {
   global.Headers = Headers;
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -319,7 +319,7 @@ export default function Dashboard() {
                       }}
                       onClick={() =>
                         router.push(
-                          `/?question=${encodeURIComponent(message.text)}`
+                          `/?question=${encodeURIComponent(message.text)}`,
                         )
                       } // <-- Add this line
                     >
