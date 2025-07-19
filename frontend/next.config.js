f7dd6da291e5cf1d944d08c1713a496ce7d63ede
Sempuri <lr.recristobal@mmdc.mcl.edu.ph>
2025-07-20 00:32:34 +0800

    for ci/cd testing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,13 +3,6 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    output: "standalone",
-  },
-  serverOptions: {
-    hostname: "0.0.0.0",
-    port: 3000,
-  },
   env: {
     NEXT_PUBLIC_API_URL: "https://brainbytes.onrender.com",
   },
