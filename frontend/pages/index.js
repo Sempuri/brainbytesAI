98c1cce1fc156e8a16f2bfb8d8f3e5cc09684952
Sempuri <lr.recristobal@mmdc.mcl.edu.ph>
2025-07-21 01:13:18 +0800

    for testing the changes in homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -234,7 +234,7 @@ export default function Home() {
           }}
         >
           <p style={{ margin: "0" }}>
-            Welcome back broski, <strong>{user.name}</strong>!
+            Welcome to BrainBytes Bro, <strong>{user.name}</strong>!
           </p>
         </div>
       )}
